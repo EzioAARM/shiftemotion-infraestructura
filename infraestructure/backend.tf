87a24ca162ae0608a95127f67313587339ae4e0a
Davidov249 <davidov249@gmail.com>
2020-07-09 01:14:36 -0600

    Creacion Infraestructura, todavia no terminado

---
+++ infraestructure/backend.tf
@@ -0,0 +1,63 @@
+#Load Balancer
+resource "aws_lb" "shiftEmotionSpotifyBlancer" {
+    name                = "shiftEmotionSpotifyBlancer"
+    internal            = false
+    load_balancer_type  = "application"
+    security_groups     = [
+        aws_security_group.sg_global.id
+    ]
+    subnets             = [
+        "${aws_subnet.public.*.id}",
+        "${aws_subnet.public2.*.id}"
+    ]
+}
+
+#Target Group
+resource "aws_lb_target_group" "shiftEmotionSpotifyTargetGroup" {
+    name                = "shiftEmotionSpotifyTargetGroup"
+    port                = 80
+    protocol            = "HTTP"
+    target_type         = "ip"
+    vpc_id              = "aws_vpc.ShiftEmotionVPC.id"
+}
+
+resource "aws_ecr_repository" "shiftEmotion" {
+    name                    = "shiftEmotionSpotify"
+    image_tag_mutability    = "MUTABLE"
+    image_scanning_configuration {
+        scan_on_push        = false
+    }
+}
+
+resource "aws_ecs_task_definition" "shiftEmotionSpotifyTask" {
+    family                  = "shiftEmotionSpotifyTask"
+    container_definitions   = <<TASK_DEFINITION
+[
+    {
+        "cpu": 0.5,
+        "memory": 128,
+        "execution_role_arn": ${var.task_policy_arn}
+        "image": ${aws_ecr_repository.shiftEmotion.repository_url},
+        "portMappings": [
+            {
+                "containerHost": 80,
+                "hostPort": 80
+            }
+        ]
+    }
+]
+TASK_DEFINITION
+}
+
+resource "aws_ecs_cluster" "shiftEmotionSpotifyCluster" {
+    name                    = "ShiftEmotionSpotifyCluster"
+    capacity_providers      = "FARGATE"
+}
+
+resource "aws_ecs_service" "SpotifyIntegrationService" {
+    name                    = "SpotifyIntegrationTask"
+    cluster                 = aws_ecs_cluster.shiftEmotionSpotifyCluster.id
+    task_definition         = aws_ecs_task_definition.shiftEmotionSpotifyTask.arn
+    desired_count           = 2
+    iam_role                = ""
+}
--- infraestructure/networking.tf
+++ infraestructure/networking.tf
@@ -6,10 +6,15 @@ resource "aws_vpc" "ShiftEmotionVPC" {
     }
 }
 
-#subnet publica
+#subnets publicas
 resource "aws_subnet" "public" {
     vpc_id          = aws_vpc.ShiftEmotionVPC.id
     cidr_block      = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "public2" {
+    vpc_id          = aws_vpc.ShiftEmotionVPC.id
+    cidr_block      = "10.0.3.0/24"
 }
 
 #subnet privada
@@ -35,6 +40,11 @@ resource "aws_route_table" "route_table" {
 #asociación de route table y subnet publica
 resource "aws_route_table_association" "public_subnet" {
     subnet_id       = aws_subnet.public.id
+    route_table_id  = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "public2_subnet" {
+    subnet_id       = aws_subnet.public2.id 
     route_table_id  = aws_route_table.route_table.id
 }
 
