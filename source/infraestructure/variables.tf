dc573eac5df96fb8aae60c76e6d791d2fcdaad00
Axel Rodriguez <alejandrom9712@gmail.com>
2020-07-14 01:56:40 -0600

    Agregar variables

---
+++ source/infraestructure/variables.tf
@@ -0,0 +1,25 @@
+variable "LambdaBuildIAMRole_arn" {}
+variable "LambdaPipelineIAMRole_arn" {}
+variable "LambdaDeployIAMRole_arn" {}
+variable "ECRDockerBuildIAMRole_arn" {}
+variable "ECRPipelineIAMRole_arn" {}
+variable "ECRDeployIAMRole_arn" {}
+variable "S3PipelineRole_arn" {}
+variable "S3DeployRole_arn" {}
+variable "WebAppBuildIAMRole_arn" {}
+variable "WebAppPipelineIAMRole_arn" {}
+variable "WebAppDeployIAMRole_arn" {}
+variable "shiftEmotionTaskRole_arn" {}
+
+variable "LambdaBuildIAMRole_id" {}
+variable "LambdaPipelineIAMRole_id" {}
+variable "LambdaDeployIAMRole_id" {}
+variable "ECRDockerBuildIAMRole_id" {}
+variable "ECRPipelineIAMRole_id" {}
+variable "ECRDeployIAMRole_id" {}
+variable "S3PipelineRole_id" {}
+variable "S3DeployRole_id" {}
+variable "WebAppBuildIAMRole_id" {}
+variable "WebAppPipelineIAMRole_id" {}
+variable "WebAppDeployIAMRole_id" {}
+variable "shiftEmotionTaskRole_id" {}
